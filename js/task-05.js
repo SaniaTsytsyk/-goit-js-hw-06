af1d5af3b7fcdf115f7d15e2ad71080e53d85d50
SaniaTsytsyk <108991553+SaniaTsytsyk@users.noreply.github.com>
2022-11-19 19:15:54 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,32 @@
-const inputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
+// const inputEl = document.querySelector('#name-input');
+// const outputEl = document.querySelector('#name-output');
 
-inputEl.onchange = function () {
-   const newValue = inputEl.value
+// inputEl.onchange = function () {
+//    const newValue = inputEl.value
 
-   newValue == "" || newValue == null
-      ? outputEl.textContent = "Anonymous"
-      : outputEl.textContent = newValue;
+//    newValue == "" || newValue == null
+//       ? outputEl.textContent = "Anonymous"
+//       : outputEl.textContent = newValue;
+// };
+
+const refs = {
+   input: document.querySelector("#name-input"),
+   nameMessage:
+      document.querySelector("#name-output"),
 };
+
+refs.nameMessage.textContent =
+   "Anonymous";
+
+refs.input.addEventListener(
+   "input", onInputChange);
+
+function onInputChange(event) {
+   if (!event.currentTarget.value) {
+      refs.nameMessage.textContent =
+         "Anonymous";
+   } else {
+      refs.nameMessage.textContent =
+         event.currentTarget.value;
+   }
+}
